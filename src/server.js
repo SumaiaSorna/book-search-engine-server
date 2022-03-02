23b992f0b77210b9b2d7c87082baede23ccca584
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-02 20:50:45 +0000

    create authMiddleware

--- src/server.js
+++ src/server.js
@@ -1,21 +1,11 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const typeDefs = require("./schema");
+// const resolvers = require("./resolvers");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const server = new ApollServer({
+//   typeDefs,
+//   resolvers,
+// });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// const init = () => {};
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../public')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// init();
