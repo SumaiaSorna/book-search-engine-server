db1890059e837072077b7c16e526a74ed1a8d651
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-02 19:45:58 +0000

    added schema

---
+++ src/schema/index.js
@@ -0,0 +1,57 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    saveBooks: [Book]
+  }
+
+type Book {
+    bookId: ID!
+    authors: [String]!
+    title: String!
+    description: String
+    image: String
+    link: String
+}
+
+ type Auth {
+     token: ID
+     user: User!
+ }
+
+ type Query {
+     me: User
+ }
+
+ input UserInput{
+     email: String!
+    password: String!
+ }
+
+ input AddUserInput{
+     username: String!
+     email: String!
+     password: String!
+ }
+
+ input SaveBookInput {
+     bookId: ID!
+     authors: [String]!
+     description: Stringtitle: String!
+     image: String
+     link: String
+ }
+
+ type Mutation{
+     login(user: UserInput!): Auth
+     addUser(user: AddUserInput!): Auth
+     saveBook(book: SaveBookInput!): User
+     removeBook(bookId: ID!): User
+ }
+`;
+
+module.exports = typeDefs;
