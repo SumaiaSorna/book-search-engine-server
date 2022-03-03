2afbd9ef5f615e5bba51609b0382d96f0509d096
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-03 10:23:18 +0000

    basic boilercode for the resolvers

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
-// const typeDefs = require("./schema");
-// const resolvers = require("./resolvers");
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
 
-// const server = new ApollServer({
-//   typeDefs,
-//   resolvers,
-// });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// const init = () => {};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// init();
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
