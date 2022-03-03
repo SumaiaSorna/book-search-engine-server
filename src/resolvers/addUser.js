f7dc7d2a633aefb281cce6d2b37f3187f85f74e7
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-03 16:39:35 +0000

    added addUser resolvers function

--- src/resolvers/addUser.js
+++ src/resolvers/addUser.js
@@ -1,3 +1,19 @@
-const addUser = () => {};
+const { ApolloError } = require("apollo-server");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const addUser = async (_, { user }) => {
+  try {
+    const newUser = await User.create(user);
+
+    return {
+      token: signToken(newUser),
+      user: newUser,
+    };
+  } catch (error) {
+    console.log(`[Error]: Failed to add user | ${error.message}`);
+    throw new ApolloError("Failed to add User");
+  }
+};
 
 module.exports = addUser;
