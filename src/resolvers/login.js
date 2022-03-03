abdd43abf769c7ea9ae3a8f8e5c31ce1bb1ea786
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-03 16:32:50 +0000

    added login resolver

--- src/resolvers/login.js
+++ src/resolvers/login.js
@@ -1,3 +1,32 @@
-const login = () => {};
+const { AuthenticationError } = require("apollo-server");
+
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const login = async (_, { input }, context) => {
+  try {
+    const user = await User.findOne({ email: input.email });
+
+    if (!user) {
+      console.log("[ERROR]: Failed to login | User does not exist");
+      throw new AuthenticationError("Failed to login");
+    }
+
+    const isValidPassword = await user.checkPassword(input.password);
+
+    if (!isValidPassword) {
+      console.log("[ERROR]: Failed to login | Incorrect password");
+      throw new AuthenticationError("Failed to login");
+    }
+
+    return {
+      token: signToken(user),
+      user: userFromDb,
+    };
+  } catch (error) {
+    console.log(`[ERROR]: Failed to login | ${error.message}`);
+    throw new AuthenticationError("Failed to login");
+  }
+};
 
 module.exports = login;
