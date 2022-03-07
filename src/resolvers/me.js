a664e0cb0fd1bc73879af3e769c7ae33285ba720
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-07 23:55:17 +0000

    refactore me resolvers

--- src/resolvers/me.js
+++ src/resolvers/me.js
@@ -1,13 +1,26 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, ApolloError } = require("apollo-server");
 
 const { User } = require("../models");
 
-const me = async (_, __, { user }) => {
-  if (user) {
-    const userFromDb = await User.findById(user.id);
-    return userFromDb;
+const me = async (_, __, context) => {
+  try {
+    if (context.user) {
+      const user = await User.findById(context.user._id);
+
+      if (!user) {
+        console.log(`[ERROR]: Failed to get user | User does not exist`);
+        throw new AuthenticationError("Failed to get user");
+      }
+
+      return user;
+    }
+
+    console.log(`[ERROR]: Failed to get user | User not logged in`);
+    throw new AuthenticationError("Failed to get user");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user | ${error.message}`);
+    throw new ApolloError("Failed to get user");
   }
-  throw new AuthenticationError("Unauthorized access");
 };
 
 module.exports = me;
