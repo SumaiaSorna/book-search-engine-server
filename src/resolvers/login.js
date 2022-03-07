a62d783549939d11865dea7af2d8091896d698f1
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-07 23:58:08 +0000

    refactor login resolvers functions

--- src/resolvers/login.js
+++ src/resolvers/login.js
@@ -1,31 +1,31 @@
-const { AuthenticationError } = require("apollo-server");
+const { ApolloError, AuthenticationError } = require("apollo-server");
 
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
-const login = async (_, { input }, context) => {
+const login = async (_, { input }) => {
   try {
-    const userFromDb = await User.findOne({ email: input.email });
+    const user = await User.findOne({ email: input.email });
 
-    if (!userFromDb) {
-      console.log("[ERROR]: Failed to login | User does not exist");
+    if (!user) {
+      console.log(`[ERROR]: Failed to login | User does not exist`);
       throw new AuthenticationError("Failed to login");
     }
 
-    const isValidPassword = await userFromDb.checkPassword(input.password);
+    const isValidPassword = await user.isCorrectPassword(input.password);
 
     if (!isValidPassword) {
-      console.log("[ERROR]: Failed to login | Incorrect password");
+      console.log(`[ERROR]: Failed to login | Incorrect password`);
       throw new AuthenticationError("Failed to login");
     }
 
     return {
-      token: signToken(userFromDb),
-      user: userFromDb,
+      token: signToken(user),
+      user,
     };
   } catch (error) {
     console.log(`[ERROR]: Failed to login | ${error.message}`);
-    throw new AuthenticationError("Failed to login");
+    throw new ApolloError("Failed to login");
   }
 };
 
